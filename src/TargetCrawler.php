2d416e2ccb337af70f4f1426b2f25ebdc768d514
a.kameli@ymail.com <git251714>
2023-02-15 09:17:45 +0330

    conposer.js edited

--- src/TargetCrawler.php
+++ src/TargetCrawler.php
@@ -9,17 +9,16 @@ class TargetCrawler
         try{
             $url = config("targetCrawler.url");
             $elements = config("targetCrawler.elements");
-
             $client = new Client(HttpClient::create(['timeout' => 60]));
             $crawler = $client->request('GET', $url);
             $targetData = [];
             $i = 1;
-            foreach ($elements as $target){
-                $targetData["data".$i]=$crawler->filter($target)->first()->text();
+            foreach ($elements as $key=>$value){
+                $targetData[$key]=$crawler->filter($value)->first()->text();
                 $i++;
             }
 
-            return $targetData["data1"];
+            return $targetData;
         }
         catch(\Exception $e)
         {
